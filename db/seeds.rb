2dc64b52b5e9adc38ebc20ac132b10cddd584fa5
motivatedHamster <fangrock@live.com>
2015-06-08 21:26:17 +0000

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Joke.create body: 'What do you call two straight days of rain in Seattle? A weekend.'
+Joke.create body: 'A newcomer to Seattle arrives on a rainy day. He gets up the next day and it\'s raining. It also rains the day after that, and the day after that. He goes out to lunch and sees a young kid and asks out of despair, "Hey kid, does it ever stop raining around here?" The kid says, "How do I know? I\'m only 6."'
+Joke.create body: 'There’s two fish in a tank. One turns to the other and says
+"You man the guns, I’ll drive"'
+
+Quote.create body: "Luck is believing you're lucky. \n-Tennessee Williams"
+Quote.create body: "Without knowing the force of words, it is impossible to know men.  \n-Confucius"
+Quote.create body: "By failing to prepare, you are preparing to fail.  \n-Benjamin Franklin"
